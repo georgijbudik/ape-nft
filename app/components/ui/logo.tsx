227f14b74eaaa3cc9ef274edaadd15f10f7d86fe
georgijbudik <georgijbudik@gmail.com>
2024-03-19 22:52:54 +0100

    feat: add logo component

---
+++ app/components/ui/logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <svg className="w-12 h-8 fill-[#1E1E1E]">
+      <use xlinkHref="/sprite/sprite.svg#icon-logo"></use>
+    </svg>
+  );
+};
+
+export default Logo;
