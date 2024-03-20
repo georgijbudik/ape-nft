2d2a2950e1ff9289ab00aaa14353cae599118fd1
georgijbudik <georgijbudik@gmail.com>
2024-03-20 16:15:57 +0100

    feat: add mind-map section styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/hero";
-import Story from "./components/story";
+import MindMap from "./components/mind-map";
+import Story from "./components/about";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col pt-14">
+    <main className="flex min-h-screen flex-col">
       <Hero />
       <Story />
+      <MindMap />
     </main>
   );
 }
