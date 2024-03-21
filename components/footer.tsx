860568fd4719eedc5cf01baaa5274fced42df8f7
georgijbudik <georgijbudik@gmail.com>
2024-03-21 16:22:39 +0100

    feat: add footer styles

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="pt-[60px] md:pt-20 flex justify-center">
+      <p className="w-[216px] md:w-auto text-center text-primary text-xs lg:text-base font-normal font-messinaSans uppercase leading-[14px] lg:leading-[19px]">
+        © Yacht ape 2024 all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
