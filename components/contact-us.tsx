5c5a3ea685baae229b23031bb613de9533db2ea0
georgijbudik <georgijbudik@gmail.com>
2024-03-21 22:57:19 +0100

    feat: smooth scroll from hero to contact us

--- components/contact-us.tsx
+++ components/contact-us.tsx
@@ -3,7 +3,10 @@ import Title from "./ui/title";
 
 const ContactUs = () => {
   return (
-    <section className="container flex flex-col items-center pt-[60px] md:pt-20 lg:pt-[120px]">
+    <section
+      id="contact-us"
+      className="container flex flex-col items-center pt-[60px] md:pt-20 lg:pt-[120px]"
+    >
       <Title>Are you in?</Title>
       <svg className="w-6 h-6 stroke-primary mb-4 md:mb-6 lg:mb-9">
         <use xlinkHref="/icons/sprite.svg#icon-cross"></use>
