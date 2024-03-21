e5403d08d85fb84f1dc0289d36a72dd22eeb8619
georgijbudik <georgijbudik@gmail.com>
2024-03-21 16:21:58 +0100

    feat: add form component

---
+++ components/ui/input.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex text-primary font-normal uppercase font-messinaSans border border-secondary px-6 py-[17px] lg:py-[22.5px] rounded-tr-xl rounded-br-xl w-full text-xs bg-transparent placeholder:text-primary, text-opacity-25 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
