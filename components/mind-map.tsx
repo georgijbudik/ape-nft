f7e3ad4e8bbd6e1cc490b3abab94d67afac648ea
georgijbudik <georgijbudik@gmail.com>
2024-03-21 22:56:14 +0100

    feat: add swiper to mind map section

--- components/mind-map.tsx
+++ components/mind-map.tsx
@@ -2,6 +2,9 @@
 import MindMapItem from "./ui/mind-map-item";
 import Pagination from "./ui/pagination";
 import Title from "./ui/title";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const mindMapItems = [
   {
@@ -19,19 +22,20 @@ const mindMapItems = [
 ];
 
 const MindMap = () => {
-  const prevImage = () => {};
-
-  const nextImage = () => {};
   return (
     <section className="container pt-[60px] md:pt-20 lg:pt-[120px]">
-      <div className="flex flex-col items-center">
-        <Title>Mind map</Title>
-        <div className="md:grid md:grid-cols-2 md:gap-6 md:justify-center">
-          {mindMapItems.map((item, index) => {
-            return (
-              <MindMapItem key={index} text={item.text} title={item.title} />
-            );
-          })}
+      <Title>Mind map</Title>
+      <div className="hidden md:grid md:grid-cols-2 md:gap-6">
+        {mindMapItems.map((item, index) => {
+          return (
+            <MindMapItem key={index} text={item.text} title={item.title} />
+          );
+        })}
+        <a
+          href="https://boredapeyachtclub.com/"
+          target="blank"
+          referrerPolicy="no-referrer"
+        >
           <div className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:p-6 bg-accent rounded-xl md:rounded-2xl lg:rounded-3xl flex-col justify-between inline-flex">
             <div className="flex justify-end pr-3 lg:pr-0">
               <svg className="w-[36px] h-[36px] lg:w-12 lg:h-12 stroke-primary">
@@ -46,10 +50,41 @@ const MindMap = () => {
               in mind map
             </div>
           </div>
-        </div>
-        <div className="md:hidden">
-          <Pagination prevImage={prevImage} nextImage={nextImage} />
-        </div>
+        </a>
+      </div>
+      <div className="md:hidden">
+        <Swiper className="mySwiper" spaceBetween={24}>
+          {mindMapItems.map((item, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <MindMapItem text={item.text} title={item.title} />
+              </SwiperSlide>
+            );
+          })}
+          <SwiperSlide>
+            <a
+              href="https://boredapeyachtclub.com/"
+              target="blank"
+              referrerPolicy="no-referrer"
+            >
+              <div className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:p-6 bg-accent rounded-xl md:rounded-2xl lg:rounded-3xl flex-col justify-between inline-flex">
+                <div className="flex justify-end pr-3 lg:pr-0">
+                  <svg className="w-[36px] h-[36px] lg:w-12 lg:h-12 stroke-primary">
+                    <use xlinkHref="/icons/sprite.svg#icon-arrow"></use>
+                  </svg>
+                </div>
+                <div className="w-48 lg:w-[456px] text-primary text-[32px] lg:text-[64px] font-black uppercase leading-8 lg:leading-[64px]">
+                  Learn
+                  <br />
+                  more
+                  <br />
+                  in mind map
+                </div>
+              </div>
+            </a>
+          </SwiperSlide>
+          <Pagination />
+        </Swiper>
       </div>
     </section>
   );
--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -1,22 +1,21 @@
-"use client";
+import { useSwiper } from "swiper/react";
 
-interface IPaginationProps {
-  prevImage: () => void;
-  nextImage: () => void;
-}
+const Pagination = () => {
+  const swiper = useSwiper();
 
-const Pagination = ({ prevImage, nextImage }: IPaginationProps) => {
   return (
     <div className="flex items-center justify-center gap-12 mt-6">
       <button
-        onClick={prevImage}
+        type="button"
         className="text-primary text-2xl font-normal font-biroScript leading-normal hover:accent transition-all duration-300"
+        onClick={() => swiper.slidePrev()}
       >
         Prev
       </button>
       <button
-        onClick={nextImage}
+        type="button"
         className="text-primary text-2xl font-normal font-biroScript leading-normal hover:accent transition-all duration-300"
+        onClick={() => swiper.slideNext()}
       >
         Next
       </button>
