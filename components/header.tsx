c5635528a8141490572077123696edf437269fc7
georgijbudik <georgijbudik@gmail.com>
2024-03-23 18:48:22 +0100

    feat: add dropdown menu

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,9 @@
-import BurgerMenu from "./header/burger-menu";
 import Logo from "./ui/logo";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetFooter,
-  SheetHeader,
-  SheetTrigger,
-} from "./ui/sheet";
+
 import HeaderSocialList from "./header/header-social-list";
-import { BURGER_MENU_LINKS } from "@/constants";
+
+import HeaderDropDown from "./header/header-dropdown";
+import MobileMenu from "./header/mobile-menu";
 
 const Header = () => {
   return (
@@ -18,45 +12,9 @@ const Header = () => {
         <Logo className="fill-secondary group-hover:fill-primary" />
       </div>
       <div className="absolute right-2 top-2 md:top-3 md:right-3 lg:top-6 lg:right-4">
-        <BurgerMenu label="MENU" />
+        <HeaderDropDown />
       </div>
-      <Sheet>
-        <div className="absolute right-2 top-2 md:top-3 md:right-3 lg:top-6 lg:right-4 md:hidden">
-          <SheetTrigger asChild>
-            <BurgerMenu label="MENU" />
-          </SheetTrigger>
-        </div>
-        <SheetContent className="bg-[#181818]">
-          <SheetHeader className="absolute top-[70px] left-4">
-            <SheetClose asChild>
-              <Logo className="fill-primary group-hover:fill-accent" />
-            </SheetClose>
-          </SheetHeader>
-          <HeaderSocialList isModalOpen />
-          <div className="flex flex-col h-full justify-between">
-            <ul className="flex flex-col items-center gap-4">
-              {BURGER_MENU_LINKS.map((link) => {
-                return (
-                  <SheetClose asChild key={link.title}>
-                    <a
-                      href={link.href}
-                      className="h-10 text-primary text-2xl font-semibold font-messinaSans leading-[29px] uppercase"
-                    >
-                      {link.title}
-                    </a>
-                  </SheetClose>
-                );
-              })}
-            </ul>
-            <SheetFooter>
-              <div className="w-[216px] text-center text-primary text-xs font-normal font-messinaSans uppercase leading-[14px]">
-                © Yacht ape 2024 all rights reserved
-              </div>
-            </SheetFooter>
-          </div>
-        </SheetContent>
-      </Sheet>
-
+      <MobileMenu />
       <HeaderSocialList />
     </header>
   );
---
+++ components/header/header-dropdown.tsx
@@ -0,0 +1,30 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { BURGER_MENU_LINKS } from "@/constants";
+import BurgerMenuBtn from "./burger-menu-btn";
+import BurgerMenuLink from "./burger-menu-link";
+
+const HeaderDropDown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <BurgerMenuBtn label="MENU" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent side="left" className="flex">
+        {BURGER_MENU_LINKS.map((link, index) => {
+          return (
+            <DropdownMenuItem className="first:rounded-l-xl last:rounded-r-xl">
+              <BurgerMenuLink key={index} label={link.title} href={link.href} />
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default HeaderDropDown;
