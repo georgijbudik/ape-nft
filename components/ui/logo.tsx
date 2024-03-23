2136358e8f9d7e152bac16c7600d7ae6938de968
georgijbudik <georgijbudik@gmail.com>
2024-03-23 12:11:29 +0100

    feat: change logo color if the modal is open

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,9 +1,15 @@
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-const Logo = () => {
+const Logo = ({ className }: { className: string }) => {
   return (
     <Link href="/" className="group">
-      <svg className="w-12 h-8 lg:w-[72px] lg:h-[50px] fill-secondary group-hover:fill-primary transition-all duration-300">
+      <svg
+        className={cn(
+          "w-12 h-8 lg:w-[72px] lg:h-[50px] transition-all duration-300",
+          className
+        )}
+      >
         <use xlinkHref="/icons/sprite.svg#icon-logo"></use>
       </svg>
     </Link>
