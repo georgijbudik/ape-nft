f34e01c17bd452ef0859f6f594bce468c3b76ded
georgijbudik <georgijbudik@gmail.com>
2024-03-23 18:50:18 +0100

    feat: add form button text change on action

--- components/contact/mint-form.tsx
+++ components/contact/mint-form.tsx
@@ -14,26 +14,15 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
-
-const formSchema = z.object({
-  username: z
-    .string()
-    .min(2, {
-      message: "Wrong discord",
-    })
-    .max(32, {
-      message: "Wrong discord",
-    })
-    .startsWith("@", {
-      message: "Wrong discord",
-    }),
-  walletAddress: z.string(),
-  //         .startsWith("1X", {
-  //     message: "Wrong address",
-  //   }),
-});
+import { formSchema } from "@/schemas";
+import { useState } from "react";
 
 const MintForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [buttonText, setButtonText] = useState<"mint" | "minted" | "error">(
+    "mint"
+  );
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -42,18 +31,36 @@ const MintForm = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     form.reset();
-  }
+    try {
+      setIsSubmitting(true);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setButtonText("minted");
+      setIsSubmitting(false);
+    } catch (error) {
+      setIsSubmitting(false);
 
+      setButtonText("error");
+    } finally {
+      setTimeout(() => {
+        setIsSubmitting(false);
+        setButtonText("mint");
+      }, 2000);
+    }
+  };
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-4 lg:space-y-6 w-full md:w-[248px] lg:w-[397px]"
+        autoComplete="off"
+      >
         <FormField
           control={form.control}
           name="username"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="relative">
               <div className="flex">
                 <FormLabel>
                   <div className="w-12 h-12 lg:w-[64px] lg:h-[64px] p-3 lg:p-5 bg-[#1E1E1E] rounded-tl-lg rounded-bl-lg justify-center items-center">
@@ -68,12 +75,13 @@ const MintForm = () => {
                     {...field}
                     className={cn(
                       "",
-                      form.formState.errors.walletAddress && "border-accent"
+                      form.formState.errors.username &&
+                        "border-accent text-accent"
                     )}
                   />
                 </FormControl>
               </div>
-              <FormMessage />
+              <FormMessage className="absolute right-0 bottom-[-14px] lg:bottom-[-16px]" />
             </FormItem>
           )}
         />
@@ -81,7 +89,7 @@ const MintForm = () => {
           control={form.control}
           name="walletAddress"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="relative">
               <div className="flex">
                 <FormLabel>
                   <div className="w-12 h-12 lg:w-[64px] lg:h-[64px] p-3 lg:p-5 bg-[#1E1E1E] rounded-tl-lg rounded-bl-lg justify-center items-center">
@@ -96,20 +104,25 @@ const MintForm = () => {
                     {...field}
                     className={cn(
                       "",
-                      form.formState.errors.walletAddress && "border-accent"
+                      form.formState.errors.walletAddress &&
+                        "border-accent text-accent"
                     )}
                   />
                 </FormControl>
               </div>
-              <FormMessage />
+              <FormMessage className="absolute right-0 bottom-[-14px] lg:bottom-[-16px]" />
             </FormItem>
           )}
         />
         <button
+          disabled={isSubmitting}
           type="submit"
-          className="w-full h-[41px] text-primary pt-2.5 pb-3 bg-accent rounded-lg justify-center items-center uppercase hover:text-secondary transition-all duration-300"
+          className={cn(
+            "w-full h-[41px] lg:h-[70px] text-primary lg:text-[28px] pt-2.5 pb-3 leading-[19px] bg-accent rounded-lg lg:rounded-xl justify-center items-center uppercase hover:text-secondary transition-all duration-300",
+            isSubmitting && "bg-[#A53041]"
+          )}
         >
-          Mint
+          {buttonText}
         </button>
       </form>
     </Form>
