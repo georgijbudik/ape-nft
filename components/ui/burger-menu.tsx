fa87bb91ef40a64e3f48674c119fa889fc59277d
georgijbudik <georgijbudik@gmail.com>
2024-03-23 12:11:45 +0100

    fix: fix burger menu button

--- components/ui/burger-menu.tsx
+++ components/ui/burger-menu.tsx
@@ -3,7 +3,7 @@ const BurgerMenu = ({ label }: { label: string }) => {
     <div className="w-12 h-12 lg:w-20 lg:h-20 bg-stone-900 bg-opacity-10 rounded-lg backdrop-blur-md justify-center items-center gap-2 inline-flex group hover:cursor-pointer">
       <button
         type="button"
-        className="text-stone-900 text-xs lg:text-base font-semibold font-messinaSans leading-[14px] group-hover:underline group-hover:decoration-1 group-hover:text-primary transition-all duration-300"
+        className="text-stone-900 text-xs lg:text-base font-semibold font-messinaSans leading-[14px] group-hover:underline group-hover:text-primary transition-all duration-300"
       >
         {label}
       </button>
