11525da0ea402b310f13002d2755d56a1319e781
georgijbudik <georgijbudik@gmail.com>
2024-03-23 12:11:01 +0100

    feat: reusable header social list and items

--- components/ui/header-social-item.tsx
+++ components/ui/header-social-item.tsx
@@ -1,11 +1,26 @@
-const HeaderSocialItem = ({ icon }: { icon: string }) => {
+import { cn } from "@/lib/utils";
+
+const HeaderSocialItem = ({
+  isModalOpen,
+  icon,
+}: {
+  isModalOpen?: boolean;
+  icon: string;
+}) => {
   return (
-    <li className="w-12 h-12 lg:w-20 lg:h-20 bg-stone-900 bg-opacity-10 rounded-lg backdrop-blur-md justify-center items-center gap-2 inline-flex group hover:cursor-pointer">
-      <button
-        type="button"
-        className="text-stone-900 text-xs lg:text-base font-semibold font-messinaSans leading-[14px]"
-      >
-        <svg className="w-4 h-4 md:w-6 md:h-6 group-hover:fill-primary transition-all duration-300">
+    <li
+      className={cn(
+        "w-12 h-12 lg:w-20 lg:h-20 bg-stone-900 bg-opacity-10 rounded-lg backdrop-blur-md justify-center items-center gap-2 inline-flex group hover:cursor-pointer",
+        isModalOpen && "bg-white"
+      )}
+    >
+      <button type="button">
+        <svg
+          className={cn(
+            "w-4 h-4 md:w-6 md:h-6 group-hover:fill-primary transition-all duration-300",
+            isModalOpen && "fill-primary group-hover:fill-accent"
+          )}
+        >
           <use xlinkHref={`/icons/sprite.svg#${icon}`}></use>
         </svg>
       </button>
---
+++ components/ui/header-social-list.tsx
@@ -0,0 +1,28 @@
+import { socialMediaItems } from "@/constants";
+import React from "react";
+import HeaderSocialItem from "./header-social-item";
+import { cn } from "@/lib/utils";
+
+const HeaderSocialList = ({ isModalOpen }: { isModalOpen?: boolean }) => {
+  return (
+    <ul
+      className={cn(
+        "absolute flex flex-col right-2 md:right-3 lg:right-4 top-[64px] md:top-[68px] lg:top-[120px] gap-2 lg:gap-4",
+        isModalOpen &&
+          "right-4 top-[118px] md:top-[122px] lg:top-[160px] md:right-4"
+      )}
+    >
+      {socialMediaItems.map((item, index) => {
+        return (
+          <HeaderSocialItem
+            isModalOpen={isModalOpen}
+            key={index}
+            icon={item.icon}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default HeaderSocialList;
